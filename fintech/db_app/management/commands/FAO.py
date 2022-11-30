749ba396e8fc8be2af6f5d722454b4586f0f3848
Volodymyr Nikulishyh <v.nikulishyn@gmail.com>
2022-11-30 09:48:53 +0200

    Rename class Work_Book into WorkBook

--- fintech/db_app/management/commands/FAO.py
+++ fintech/db_app/management/commands/FAO.py
@@ -12,8 +12,7 @@ def format_data_in_tuple(tuple_):
 
 
 def are_ends_of_range_int(ends_of_range: dict):
-    if isinstance(ends_of_range['min'], int) and isinstance(
-                    ends_of_range['max'], int):
+    if isinstance(ends_of_range["min"], int) and isinstance(ends_of_range["max"], int):
         return True
     else:
         logging.error(f"The ends of the range {ends_of_range} are not integer.")
@@ -21,14 +20,14 @@ def are_ends_of_range_int(ends_of_range: dict):
 
 
 def are_ends_of_range_positive(ends_of_range: dict):
-    if ends_of_range['min'] >= 0 and ends_of_range['max'] >= 0:
+    if ends_of_range["min"] >= 0 and ends_of_range["max"] >= 0:
         return True
     else:
         return False
 
 
 def is_max_more_than_min(ends_of_range: dict):
-    if ends_of_range['max'] >= ends_of_range['min']:
+    if ends_of_range["max"] >= ends_of_range["min"]:
         return True
     else:
         return False
@@ -42,13 +41,14 @@ def is_list_contains(List1, List2):
     else:
         return False
 
+
 def get_property_name(row_col_variable):
-    min_ = f'min_{row_col_variable}'
-    max_ = f'max_{row_col_variable}'
+    min_ = f"min_{row_col_variable}"
+    max_ = f"max_{row_col_variable}"
     return min_, max_
 
 
-class Work_Book:
+class WorkBook:
     def __init__(self, path):
         self.path = path
 
@@ -97,27 +97,30 @@ class RangeSetter:
         assert is_max_more_than_min(self.auto_ends), assert_comment2
 
     def _custom_ends_assert_testing(self):
-        assert self.custum_ends, "custum_ends is not valid. The variable should be dictionary with min, max keys"
-    
+        assert (
+            self.custum_ends
+        ), "custum_ends is not valid. The variable should be dictionary with min, max keys"
+
     def _get_ranges(self):
         self._custom_ends_assert_testing(self)
-        custom_range = range(self.custum_ends['min'],
-                             self.custum_ends['max'] + 1)
+        custom_range = range(self.custum_ends["min"], self.custum_ends["max"] + 1)
 
         self._auto_ends_assert_testing()
-        auto_range = range(self.auto_ends['min'], self.auto_ends['max'] + 1)
+        auto_range = range(self.auto_ends["min"], self.auto_ends["max"] + 1)
         return custom_range, auto_range
-    
+
     def _get_ends_of_range(self):
         custom_range, auto_range = self._get_ranges()
         if is_list_contains(list(custom_range), list(auto_range)):
             return self.custum_ends
         else:
-            logging.warning(f"The custom range [{self.custum_ends['min']},"
-                            f"{self.custum_ends['max']}] exceeds hard limit"
-                            f"[{self.auto_ends['min']},{self.auto_ends['max']}]"
-                            ". Was returned automatic values based on current "
-                            "worksheet instead.")
+            logging.warning(
+                f"The custom range [{self.custum_ends['min']},"
+                f"{self.custum_ends['max']}] exceeds hard limit"
+                f"[{self.auto_ends['min']},{self.auto_ends['max']}]"
+                ". Was returned automatic values based on current "
+                "worksheet instead."
+            )
             return self.auto_ends
 
 
@@ -137,9 +140,9 @@ class TableRanges(ABC):
     def _get_auto_ends(self, row_col_variable):
         min_, max_ = get_property_name(row_col_variable)
         auto_ends = {
-                              'min': getattr(self.worksheet, min_),
-                              'max': getattr(self.worksheet, max_),
-                              }
+            "min": getattr(self.worksheet, min_),
+            "max": getattr(self.worksheet, max_),
+        }
         return auto_ends
 
     def _get_custom_ends(self, row_col_variable):
@@ -147,21 +150,22 @@ class TableRanges(ABC):
         if self.row_col_ranges_dict:
             try:
                 custom_ends = {
-                                        'min': self.row_col_ranges_dict[min_],
-                                        'max': self.row_col_ranges_dict[max_],
-                                        }
+                    "min": self.row_col_ranges_dict[min_],
+                    "max": self.row_col_ranges_dict[max_],
+                }
             except KeyError as err:
-                logging.warning("KeyError: The key of row_col_ranges_dict "
-                                f"variable is not correct:{err}. "
-                                "Instead custom values was returned automatic "
-                                "values based on current worksheet instead.")
+                logging.warning(
+                    "KeyError: The key of row_col_ranges_dict "
+                    f"variable is not correct:{err}. "
+                    "Instead custom values was returned automatic "
+                    "values based on current worksheet instead."
+                )
                 custom_ends = None
 
         else:
             custom_ends = None
         return custom_ends
 
-    
     def _get_range(self, row_col_variable):
         auto_ends = self._get_auto_ends(row_col_variable)
         custom_ends = self._get_custom_ends(row_col_variable)
@@ -178,24 +182,25 @@ class TableRanges(ABC):
     def max(self):
         pass
 
+
 class RowRange(TableRanges):
     @property
     def min(self):
-        return self._get_range('row')['min']
+        return self._get_range("row")["min"]
 
     @property
     def max(self):
-        return self._get_range('row')['max']
+        return self._get_range("row")["max"]
 
 
 class ColRange(TableRanges):
     @property
     def min(self):
-        return self._get_range('column')['min']
+        return self._get_range("column")["min"]
 
     @property
     def max(self):
-        return self._get_range('column')['max']
+        return self._get_range("column")["max"]
 
 
 class TableData:
@@ -230,10 +235,11 @@ class TableData:
 
 
 if __name__ == "__main__":
-    path = "/home/vnikulishyn/projects/git_hub/fintech_dajango/fintech/data/accounts.xlsx"
-    workbook_obj = Work_Book(path)
+    path = (
+        "/home/vnikulishyn/projects/git_hub/fintech_dajango/fintech/data/accounts.xlsx"
+    )
+    workbook_obj = WorkBook(path)
     worksheet_obj = WorkSheet(workbook_obj)
     data_object = TableData(worksheet_obj)
     data = data_object.get_data_from_worksheet()
     print(data)
-
